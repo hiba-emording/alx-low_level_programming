1431ff5dae010b24638567f2bc0e092ba12ee8be
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-07 05:50:30 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program use puts function to print a message and return 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    /* Using puts function to print */
+Puts("\"Programming is like building a multilingual puzzle");
+    /* Return: Always 0 (Success) */
+return (0);
+}
+
