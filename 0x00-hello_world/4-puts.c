6b196d40aa3758a416d47d245a85a27ef60a9e73
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-07 05:52:40 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 int main(void)
 {
     /* Using puts function to print */
-Puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
     /* Return: Always 0 (Success) */
 return (0);
 }
