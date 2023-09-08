e2896b75d5651df65f1099c82e312623057aef2b
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-08 14:46:25 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints all single-digit numbers of base 10,
+ * starting from 0 and followed by a new line.
+ * Using putcher function only twice.
+ *
+ * Return: Always 0 to indicate success.
+ */
+int main(void)
+
+{
+
+char digit = '0';
+
+while (digit <= '9')
+{
+putchar(digit);
+digit++;
+}
+
+putchar('\n');
+
+return (0);
+}
+
