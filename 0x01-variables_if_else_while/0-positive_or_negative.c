972ae092e0e723e71cdfbe6eadbbac58cd69fa43
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-08 13:15:05 -0700

    updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
-*The main function is where the program starts.
-*This program uses if functions to test the number type and return 0.
-*Return = Always 0 to indicate success.
-*/
+ * main - Entry point of the program
+ *
+ * This program generates a random number, checks sign, and prints the result.
+ * Return: Always 0 to indicate success.
+ */
+
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0) {
+if (n > 0)
+{
 printf("%d is positive\n", n);
 }
-else if (n < 0) {
+else if (n < 0)
+{
 printf("%d is negative\n", n);
 }
-else {
+else
+{
 printf("%d is zero\n", n);
 }
 return (0);
