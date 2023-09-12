c90fcb29c69458d38304b0f9263401c0c01983a4
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-12 12:13:32 -0700

    its not a clean code, whatever

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry point of the program.
  *
@@ -11,25 +10,41 @@
  *
  * Return: 0 (Success).
  */
-
 int main(void)
-
 {
 int i;
 unsigned long fibonacci_1 = 1, fibonacci_2 = 2, next;
-
-for (i = 0; i < 98; i++)
-
+unsigned long fibonacci_1_half1, fibonacci_1_half2,
+fibonacci_2_half1, fibonacci_2_half2;
+unsigned long half1, half2;
+for (i = 0; i < 92; i++)
 {
-printf("%lu", fibonacci_1);
-if (i < 97)
-printf(", ");
-
 next = fibonacci_1 + fibonacci_2;
+printf("%lu, ", next);
 fibonacci_1 = fibonacci_2;
 fibonacci_2 = next;
 }
-
+fibonacci_1_half1 = fibonacci_1 / 10000000000;
+fibonacci_2_half1 = fibonacci_2 / 10000000000;
+fibonacci_1_half2 = fibonacci_1 % 10000000000;
+fibonacci_2_half2 = fibonacci_2 % 10000000000;
+for (i = 93; i < 99; i++)
+{
+half1 = fibonacci_1_half1 + fibonacci_2_half1;
+half2 = fibonacci_1_half2 + fibonacci_2_half2;
+if (fibonacci_1_half2 + fibonacci_2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+printf("%lu%lu", half1, half2);
+if (i != 98)
+printf(", ");
+fibonacci_1_half1 = fibonacci_2_half1;
+fibonacci_1_half2 = fibonacci_2_half2;
+fibonacci_2_half1 = half1;
+fibonacci_2_half2 = half2;
+}
 printf("\n");
 return (0);
 }
