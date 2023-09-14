4919c4c09607844f28899cadc0f88c7e0feba28e
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-14 07:23:03 -0700

    should work

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,17 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		n = -n;
 	}
-	if (n / 10 != 0)
+	else
 	{
-		print_number(n / 10);
+		n = n;
 	}
+		if (n / 10 != 0)
+		{
+			print_number(n / 10);
+		}
+
 _putchar((n % 10) + '0');
 }
