bb6cd19ad5174a566cdcc610c282d2c5061148c0
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-18 05:41:57 -0700

    fixed: task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,26 +10,29 @@ void puts_half(char *str)
 {
 
 int length = 0;
-int start;
 int i;
 
 	while (str[length] != '\0')
 	{
 		length++;
 	}
+
 	if (length % 2 == 0)
 	{
-		start = length / 2;
-	}
-	else
-	{
-		start = (length - 1) / 2;
+		for (i = length / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 
-	for (i = start; i < length; i++)
+	else
 	{
-		_putchar(str[i]);
+		for (i = (length + 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
+
 
 _putchar('\n');
 
