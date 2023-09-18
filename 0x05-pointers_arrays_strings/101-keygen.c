bd973ceb44a804f31411052bf876b9321ee9e4db
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-09-18 14:44:06 -0700

    if you know you know

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,48 +4,26 @@
 
 /**
  * main - Entry point.
- * Description: a program that generates random valid passwords,
+ * Description: A program that generates random valid passwords
  * for the program 101-crackme.
  * Return: 0 for success.
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-const char *str;
-size_t len;
-
-extern unsigned long checksum_asm(*str, len);
+ */
 
 int main(void)
 
 {
-
+int sum = 0;
+char c;
 srand(time(NULL));
-char password[12];
-int i;
-unsigned long sum;
 
-while (1)
-{
-
-	for (i = 0; i < 11; i++)
+	while (sum <= 2645)
 	{
-		password[i] = rand() % 94 + 33;
+		c = rand() % 120;
+		sum += c;
+		putchar(c);
 	}
 
-password[11] = '\0';
-sum = checksum_asm(password, 11);
-
-	if (sum == 0xad4)
-	{
-		printf("Password: %s\n", password);
-		break;
-	}
-
-}
+putchar(2772 - sum);
 
 return (0);
-
 }
