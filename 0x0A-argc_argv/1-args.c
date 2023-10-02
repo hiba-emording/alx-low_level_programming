d233406d17772a6c98f3bda9724118e5b872eca4
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-02 14:39:22 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+_putchar((char)(argc - 1 + '0'));
+
+_putchar('\n');
+
+return (0);
+}
