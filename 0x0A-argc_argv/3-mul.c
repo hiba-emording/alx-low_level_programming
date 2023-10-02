f2f8be05eb75a4d608971724e9373dacdb14a2b7
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-02 15:23:40 -0700

    please work

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+int n1;
+int n2;
+int result;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+result = n1 *n2;
+printf("%d\n", result);
+return (0);
+}
