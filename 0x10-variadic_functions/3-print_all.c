da42ea769a46a9e7b1bad7fee771fb7d7dc3b426
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-12 05:18:19 -0700

    Fixed else prohibted error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,42 +9,43 @@
 
 void print_all(const char * const format, ...)
 {
-va_list args;
-char *separator = "";
 unsigned int i = 0;
+char *separator = "";
+char *string_arg;
+va_list args;
 
 	va_start(args, format);
-	while (format && format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-			printf("%s%c", separator, va_arg(args, int));
-			break;
-			case 'i':
-			printf("%s%d", separator, va_arg(args, int));
-			break;
-			case 'f':
-			printf("%s%f", separator, va_arg(args, double));
-			break;
-
-			case 's':
+			switch (format[i])
 			{
-				char *str = va_arg(args, char *);
+				case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+				case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+				case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+				case 's':
 
-				if (str == NULL)
-				{
-					printf("%s(nil)", separator);
-				}
-				else
-				{
-					printf("%s%s", separator, str);
-				}
+				string_arg = va_arg(args, char *);
+
+				if (!string_arg)
+				string_arg = "(nil)";
+				printf("%s%s", separator, string_arg);
+				break;
+
+				default:
+				i++;
+				continue;
 			}
-			break;
+			separator = ", ";
+			i++;
 		}
-		separator = ", ";
-		i++;
 	}
 	printf("\n");
 	va_end(args);
