a2054450b35c0ef7fdfd93896c98c7d6d7d6a987
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-19 08:44:13 -0700

    assembly program -task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+    format db `Hello, Holberton\n`, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    mov edi, format
+    xor eax, eax
+    call printf
+
+    mov eax, 60
+    xor edi, edi
+    syscall
