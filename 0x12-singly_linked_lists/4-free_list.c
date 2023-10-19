53fe3277d4a7979161690903d5f420e0b01ec003
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-10-19 04:58:36 -0700

    Free - task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees list.
+ * @head: A pointer to the head of the list.
+ */
+
+void free_list(list_t *head)
+{
+list_t *current, *next;
+current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
