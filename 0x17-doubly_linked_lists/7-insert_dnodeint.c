2171e5a77d5d29eeeef1c5920e8a7c06b00470b2
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-12-07 12:36:22 -0800

    Fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,27 +25,27 @@ if (head != NULL)
 	while (head->prev != NULL)
 	{
 		head = head->prev;
-		while (head != NULL)
+	}
+while (head != NULL)
+{
+	if (i == idx)
+	{
+		if (head->next == NULL)
 		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-				{
-					new = add_dnodeint_end(h, n);
-				}
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-					new->n = n, new->next = head->next, new->prev = head;
-					head->next->prev = new, head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next, i++;
+			new = add_dnodeint_end(h, n);
 		}
+		else
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new != NULL)
+			{
+				new->n = n, new->next = head->next, new->prev = head;
+				head->next->prev = new, head->next = new;
+				}
+			}
+			break;
+		}
+		head = head->next, i++;
 	}
 }
 }
